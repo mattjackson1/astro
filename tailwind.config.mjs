e2f5e19aeaa80147d284ce4bc8d5006a31f2319e
Matt Jackson <matt.jackson@idoxgroup.com>
2024-05-07 09:47:01 +0100

    wip:

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+		colors: {
+			'cobalt': {
+			  DEFAULT: '#0A1F8F',
+			  50: '#E0EBFC',
+			  100: '#C9DDF9',
+			  200: '#9BC0F4',
+			  300: '#6DA3EF',
+			  400: '#3F86EA',
+			  500: '#186ADE',
+			  600: '#1354B0',
+			  700: '#0E3E82',
+			  800: '#092854',
+			  900: '#041226'
+			}
+		  },
+		},
 	},
 	plugins: [],
 }
