7c24b8135eef2cbd2630563d2fbb0f1e74cbb1b1
Matt Jackson <matt.jackson@idoxgroup.com>
2024-05-08 11:21:58 +0100

    wip:

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,5 +11,43 @@ export default {
 		  },
 		},
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/forms'),
+		function ({ addComponents }) {
+			addComponents({
+				'input, select': {
+					transitionDuration: '200ms',
+					'&:not(:disabled):hover': {
+						borderColor: '#6b7280',
+					}
+				},
+				'[type="checkbox"]': {
+					width: '1.5rem',
+					height: '1.5rem',
+					color: '#2563eb',
+					borderColor: '#d1d5db',
+					borderRadius: '0.25rem',
+					'&:disabled': {
+						color: '#d1d5db'
+					},
+					'&:not(:disabled):hover': {
+						cursor: 'pointer'
+					}
+				},
+				'[type="text"]': {
+					fontSize: '0.875rem',
+					padding: '0.5rem 0.75rem',
+					borderColor: '#d1d5db',
+					'&:disabled': {
+						background: '#f3f4f6',
+						color: '#6b7280',
+					}
+				},
+				'th, td': {
+					padding: '0.5rem',
+				}
+				
+			});
+		},
+	],
 }
