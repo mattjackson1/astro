c255c0acc0833148873500565348df567d00e034
Matt Jackson <matt.jackson@idoxgroup.com>
2024-05-09 16:05:49 +0100

    installed react and alpineJS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
-
+import react from '@astrojs/react';
+import alpine from '@astrojs/alpinejs';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -13,5 +14,5 @@ export default defineConfig({
   // called `static` instead.
   publicDir: 'static',
 
-  integrations: [tailwind()]
+  integrations: [react(),alpine(),tailwind()]
 });
---
+++ src/components/ReactComponent.jsx
@@ -0,0 +1,5 @@
+export default function Test() {
+  return (
+    "React is installed"
+  )
+}
